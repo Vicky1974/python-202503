a4a3bd1fb550f0669996a2c11f7bb3c2937445d4
Vicky1974 <shuhui0702@gmail.com>
2025-04-22 20:25:53 +0800

    Create ex6-7.py

---
+++ ex6-7.py
@@ -0,0 +1,19 @@
+x = 1
+while x <= 5:
+    print(x)
+    x += 1
+'''
+'''
+x= 1
+while x <= 5:
+    if x == 3:
+       break
+    print(x)
+    x += 1
+'''    
+x= 0
+while x < 5:
+    x += 1
+    if x == 3:
+       continue
+    print(x)
