1c9cefc0ee5057cb5caba9678444450574500c8c
Vicky1974 <shuhui0702@gmail.com>
2025-04-22 20:44:17 +0800

    Create ex6-8.py

---
+++ ex6-8.py
@@ -0,0 +1,11 @@
+'''
+輸入2個整數,求兩數的和並輸出
+輸入2個整數,求兩數的平方和並輸出
+'''
+'''
+
+x = int(input("please input num x: "))
+y = int(input("please input num y: "))
+print("x = ",x)
+print("y = ",y)
+print("x + y =",x + y)
