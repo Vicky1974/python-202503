0c03bfa6f932677f6e1783a798b7af2d4aaeba48
Vicky1974 <shuhui0702@gmail.com>
2025-05-06 19:55:12 +0800

    Create ex8-3.py

---
+++ ex8-3.py
@@ -0,0 +1,13 @@
+'''
+def area_fun(h,w):
+    return h * w
+h = int(input("please input height :"))
+w = int(input("please input width :"))
+print("area=",area_fun(h,w))
+
+'''
+def cal_min(a,b):
+    return a * 60 + b
+a = int(input("please input num a (hr) :"))
+b = int(input("please input num b (min):"))
+print("x = ",cal_min(a,b))
