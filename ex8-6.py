afafbac8ac0c95da47ffdd45123e45b59d6f5d62
Vicky1974 <shuhui0702@gmail.com>
2025-05-06 20:30:57 +0800

    Create ex8-6.py

---
+++ ex8-6.py
@@ -0,0 +1,17 @@
+cars = ["Ford", "Volvo", "BMW"]
+#print(cars[0])
+#print(cars[1])
+#print(cars[2])
+print(cars)
+'''
+for i in range(0,3):
+    print(cars[i])
+
+for i in cars:
+    print(i)
+print(len(cars))
+'''
+cars.append("Honda")
+print(cars)
+cars.pop(0)
+print(cars)
